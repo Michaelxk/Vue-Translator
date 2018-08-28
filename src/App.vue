f00568739179acd9ca84c4a3c4b80269733d78e3
Michaelxk <linares.michael@gmail.com>
2018-08-28 12:49:45 -0400

    Add files components for the first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+     <h1>Word Translator</h1>
+     <h5>Powerd By Vue.js</h5>
+     <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+
+export default {
+  name: 'App',
+  components: {
+    TranslateForm
+  },
+  methods: {
+    translateText:function(text){
+      alert(text);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="translateForm">
+     <form v-on:="formSubmit">
+       <input type="text" v-model="textToTranslate" placeholder="Enter a Word">
+       <input type="submit" value="Translate">   
+     </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TranslateForm',
+  data() {
+      return {
+          textToTranslate: ''
+      }
+  },
+  methods: {
+      formSubmit(e) {
+          this.$emit('formSubmit', this.textToTranslate)
+          e.preventDefault();
+      }
+  }
+}
+</script>
+
+<style>
+</style>
